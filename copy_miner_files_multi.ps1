935d020f0dc1270e084b60fd873578cd65ebba71
cafcwest <cafcwest@gmail.com>
2018-09-12 13:35:34 -0400

    Add files via upload

---
+++ copy_miner_files_multi.ps1
@@ -0,0 +1,14 @@
+﻿#Create C:\miners and copy miner files
+
+$computers = Get-Content "$PSScriptRoot\computers.txt"
+
+foreach ($computer in $computers){
+
+#    Invoke-Command -ComputerName $computer -ScriptBlock { Remove-Item "c:\miners" -Force -Recurse, New-Item -ItemType Directory "c:\miners" -Force }
+    Invoke-Command -ComputerName $computer -ScriptBlock { Remove-Item "c:\miners" -Force -Recurse }
+
+#    $to = "\\$computer\c$\yo"
+#    $session = New-PSSession -ComputerName $computer
+    Copy-Item $PSScriptRoot\miners -Destination \\$computer\c$ -Recurse -Force
+
+       }
